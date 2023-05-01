99fce7d62d6b03a1bdc84c50ab3a7464d77bd60a
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:20:19 +0100

    Committing files

---
+++ CodeForces/GNU C++17/546A | Soldier and Bananas/161589207.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+#include <algorithm>
+ 
+using namespace std ;
+#define ll long long
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+int t=1;
+ 
+    while (t--){
+        int k,n,w;
+        cin>>k>>n>>w;
+        int sum=(1+w)*w/2;
+        cout<<max((k*sum)-n,0);
+ 
+        }
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
