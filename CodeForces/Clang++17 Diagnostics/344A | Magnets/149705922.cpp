62f13ddb08dc40a8a67f5212a3292f79d5c6e543
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:22:38 +0100

    Committing files

---
+++ CodeForces/Clang++17 Diagnostics/344A | Magnets/149705922.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main()
+{
+   // ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+ 
+    int n;
+    int r=1;
+    cin>>n;
+    int tab[n];
+    for (int i=0;i<n;i++){cin>>tab[i];}
+    for (int i=0;i<n-1;i++){
+        if (tab[i]!=tab[i+1]){
+            r++;
+        }
+    }
+    cout<<r;
+    return 0;
+}
