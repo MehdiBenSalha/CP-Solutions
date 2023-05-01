70212b2c15e8dbead23621c282a2d91ff648af2b
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:22:27 +0100

    Committing files

---
+++ CodeForces/GNU C++17/41A | Translation/149895682.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+   string s,t;
+   cin >>s>>t;
+    reverse(s.begin(),s.end());
+    if(s==t){cout<<"YES";}else
+        cout<<"NO";
+}
