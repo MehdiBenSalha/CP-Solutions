89e42a5347df56bc0d652853a52d593a21645c58
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:24:07 +0100

    Committing files

---
+++ CodeForces/GNU C++17/41A | Translation/139429536.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "string"
+#include "bits/stdc++.h"
+using namespace std;
+int main() {
+    string s1,s2;
+ 
+    cin>>s1;
+    cin>>s2;
+   reverse(s2.begin(),s2.end());
+ 
+    if (s2==s1){
+        cout<<"YES";
+    } else{
+        cout<<"NO";
+    }
+    return 0;
+}
