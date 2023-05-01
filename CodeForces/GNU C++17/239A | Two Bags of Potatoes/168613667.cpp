0d1e5f17e59dcc6175748fddfef9884e1d55496b
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:19:13 +0100

    Committing files

---
+++ CodeForces/GNU C++17/239A | Two Bags of Potatoes/168613667.cpp
@@ -0,0 +1,74 @@
+#include "bits/stdc++.h"
+#include <algorithm>
+using namespace std ;
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+ 
+ 
+ 
+bool isPrime(int n){
+ 
+    for (int i = 2; i*i<=n ; ++i) {
+        if(n%i==0) return false;
+    }
+    return true;
+}
+ 
+int sumDigit(int n){
+    int sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
+}
+bool isPower2(int n){
+    double x= log2(n);
+    if(x- floor(x)==0) return true;
+    else return false;
+}
+int smallestPower(int n){
+    int x=(int ) log2(n);
+    return pow(2,x);
+}
+bool isInt(double n){
+    if(ceil(n)== floor(n)) return true;
+    else return false;
+}
+ 
+ 
+int di[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+int dj[] = { 0, 0, 1, -1, 1, -1, -1, 1 };
+ 
+ 
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    //cin>>t;
+    while (t--) {
+ 
+    ll y,k,n;
+    cin>>y>>k>>n;
+    ll kk;
+    kk=ceil((double) y/k);
+    ll kkk=kk*k;
+ 
+ 
+ 
+        if(kkk>n || y==n) cout<<-1;
+        else {
+            while(kkk<=n){
+                if(kkk-y==0){ kkk+=k; if(kkk>n) {cout<<-1; break;} }
+                else {cout<<kkk-y<<" ";
+                kkk+=k;}
+            }
+        }
+    }
+ 
+}
+ 
