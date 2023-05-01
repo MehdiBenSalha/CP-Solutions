05ef80362b43b12eae84cde9a027045abea75520
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:18:57 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1077C | Good Array/169319170.cpp
@@ -0,0 +1,94 @@
+#include "bits/stdc++.h"
+#include <algorithm>
+using namespace std ;
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+ 
+ 
+ 
+bool isPrime(int n){
+ 
+    for (int i = 2; i*i<=n ; ++i) {
+        if(n%i==0) return false;
+    }
+    return true;
+}
+int sumDigit(int n){
+    int sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
+}
+bool isPower2(int n){
+    double x= log2(n);
+    if(x- floor(x)==0) return true;
+    else return false;
+}
+int smallestPower(int n){
+    int x=(int ) log2(n);
+    return pow(2,x);
+}
+bool isInt(double n){
+    if(ceil(n)== floor(n)) return true;
+    else return false;
+}
+ 
+bool isGood(string s,int k){
+    for (int i = 0; i <= k; ++i) {
+        string d= to_string(i);
+ 
+        if(s.find(d)==string::npos) return false;
+    }
+ 
+    return true;
+}
+int di[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+int dj[] = { 0, 0, 1, -1, 1, -1, -1, 1 };
+ 
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    //cin>>t;
+    while (t--) {
+     int n;
+ 
+     cin>>n;
+ 
+    ll tab[n];
+    ll c=0,s=0;
+    map<ll,int> f;
+        for (int i = 0; i < n; ++i) {
+            cin>>tab[i];
+            f[tab[i]]++;
+            s+=tab[i];
+        }
+ 
+        vector<int> v;
+        ll cnt=0;
+        for (int i = 0; i < n; ++i) {
+            ll sum=s-tab[i];
+            if(sum%2==0){
+                ll x=sum/2;
+                if((f[x]>0 && tab[i]!=x) || (tab[i]==x && f[x]>1)) {cnt++; v.push_back(i+1);  }}
+        }
+ 
+ 
+        if(cnt==0) cout<<0;
+        else cout<<cnt<<endl;
+        for (int i = 0; i < v.size(); ++i) {
+            cout<<v[i]<<" ";
+        }
+     }
+ 
+}
+ 
+ 
+ 
+ 
