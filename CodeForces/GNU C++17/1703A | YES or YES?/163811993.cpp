b27acad447ace31bde9868cf20c6d768fba9f71f
Mehdi Ben Salha <94847838+MehdiBenSalha@users.noreply.github.com>
2023-05-01 12:19:42 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1703A | YES or YES?/163811993.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+#include <algorithm>
+ 
+using namespace std ;
+#define ll long long
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while (t--){
+ 
+        string su;
+        cin>>su;
+        transform(su.begin(), su.end(), su.begin(), ::toupper);
+        if(su=="YES") cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+ 
+ 
+ 
+}
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
